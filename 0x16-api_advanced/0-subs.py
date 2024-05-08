2a02896418df6ec69940c6730776ae3885abef98
Olaniyi2959 <olaniyiafolayan@yahoo.com>
2024-05-08 16:52:39 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,33 +1,31 @@
 #!/usr/bin/python3
-"""
-Function that queries reddit api and returns tota number of subscribers
-using the requests library and the sys.args lib
+"""Query Reddit API to determine subreddit sub count
 """
 
 import requests
 
-headers = {"user-agent": (
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-    "AppleWebKit/537.36 (KHTML, like Gecko) "
-    "Chrome/124.0.0.0 Safari/537.36"
-)}
-
 
 def number_of_subscribers(subreddit):
+    """Request number of subscribers of subreddit
+    from Reddit API
     """
-    This function returns the total number of subscribers that a subreddit has
-    and returns 0 if the subreddit doesn't exists
+    # set custom user-agent
+    user_agent = '0x16-api_advanced-jmajetich'
+    url = 'https://www.reddit.com/r/{}.json'.format(subreddit)
 
-    Args:
-        param:str : name of subreddit
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about/.json"
-    r = requests.get(url, headers=headers)
-    if r.status_code == 200:
-        data = r.json()
-        for key, value in data.items():
-            if key == "data":
-                for k, v in value.items():
-                    if k == "subscribers":
-                        return v
-    return (0)
+    # custom user-agent avoids request limit
+    headers = {'User-Agent': user_agent}
+
+    r = requests.get(url, headers=headers, allow_redirects=False)
+
+    if r.status_code != 200:
+        return 0
+
+    # load response unit from json
+    data = r.json()['data']
+    # extract list of pages
+    pages = data['children']
+    # extract data from first page
+    page_data = pages[0]['data']
+    # return number of subreddit subs
+    return page_data['subreddit_subscribers']
